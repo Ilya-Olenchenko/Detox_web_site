6c0c7be40a42a7d43ee68f27d99860c5e7d9009a
Ilya-Olenchenko <olenchenko.ilya@ukr.net>
2022-07-04 19:50:39 +0300

    адаптация тлф

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,26 @@
-jQuery(function ($) {
-    $('.mouse').fadeOut(0);
-    setTimeout(visualization_mouse, 2000);
-});
 
-function visualization_mouse() {
-    $('.mouse').fadeIn(1000);
+const windowOuterWidth = window.outerWidth
+const windowOuterHeight = window.outerHeight
+
+if(windowOuterWidth < 500 || windowOuterHeight < 500){
+    $('.mouse').fadeOut(0);
+}
+else{
+    jQuery(function ($) {
+        $('.mouse').fadeOut(0);
+        setTimeout(visualization_mouse, 2000);
+    });
+    
+    function visualization_mouse() {
+        $('.mouse').fadeIn(1000);
+    }
+    
+    document.addEventListener('wheel', (evt) => {
+        if (evt.deltaY) {
+            $('.mouse').fadeOut(1000);
+        }
+    });
 }
 
-document.addEventListener('wheel', (evt) => {
-    if (evt.deltaY) {
-        $('.mouse').fadeOut(1000);
-    }
-});
+
+
