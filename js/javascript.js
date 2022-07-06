2c10897e5c492b06d60f027dbeb18cd10d9313a4
Ілля <olenchenko.ilya@ukr.net>
2022-07-06 17:34:52 +0300

    тест  preloaderА

--- js/javascript.js
+++ js/javascript.js
@@ -21,3 +21,14 @@ else {
         }
     });
 }
+
+
+window.onload = function() {
+
+    setTimeout(function() {
+
+        document.getElementById("preloader_malc").style.display = "none";
+
+    }, 400);
+
+};
